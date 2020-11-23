9dd2b95efc33b687f3f1fffda8bbfc36fbd1888e
jessicafarias <jessica.farias.rosado@gmail.com>
2020-11-23 12:49:36 -0600

    Create HTML template
    Adding HTMLWebpackPlugin
    Create new template.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins:[
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template:'./src/template.html',
+      templateParameters:{
+        title: 'Title',
+      }
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
